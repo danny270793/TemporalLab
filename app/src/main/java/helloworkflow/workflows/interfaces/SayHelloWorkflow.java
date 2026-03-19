f7196259e5da3468754f693527be16d4af5319fc
Danny Vaca <danny270793@icloud.com>
2026-03-19 14:52:12 -0500

    alow to set language from outside

--- app/src/main/java/helloworkflow/workflows/interfaces/SayHelloWorkflow.java
+++ app/src/main/java/helloworkflow/workflows/interfaces/SayHelloWorkflow.java
@@ -3,6 +3,7 @@ package helloworkflow.workflows.interfaces;
 import io.temporal.workflow.WorkflowInterface;
 import io.temporal.workflow.WorkflowMethod;
 import io.temporal.workflow.QueryMethod;
+import io.temporal.workflow.SignalMethod;
 
 /**
  * Workflow to orchestrate the activities.
@@ -11,6 +12,9 @@ import io.temporal.workflow.QueryMethod;
 public interface SayHelloWorkflow {
     @WorkflowMethod
     String sayHello(String name);
+
+    @SignalMethod
+    void setLanguage(String language);
 
     @QueryMethod
     boolean hasObtainedTheName();
